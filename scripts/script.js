394447a925c95cae39df5c92d138707043842255
Joonnnaas <jonas.dorfinger@gmx.at>
2019-03-12 07:39:11 +0100

    added startAnimation

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,18 @@
-let y = "virtual skiarea navigation";
 
-alert(y.length);
+function init() {
+  const text = "virtual skiarea navigation";
+  let i = 0;
+  let content = "";
+  let h = document.getElementById('headline');
+
+  setInterval(() => {
+    h.textContent = "";
+    content += text.charAt(i);
+    h.textContent += content;
+
+    for (let j = 0; j < text.length - i - 1; j++) {
+      h.textContent += "_";
+    }
+    i++;
+  }, 200);
+}
