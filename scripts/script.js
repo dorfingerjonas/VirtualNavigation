c125599ad8cba685283ffbadb7a23fb2c89db826
Joonnnaas <36422131+Joonnnaas@users.noreply.github.com>
2019-03-13 21:47:02 +0100

    added sign up

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,9 @@
-
 function init() {
-  const text = "virtual skiarea navigation";
-  let element = document.getElementById('headline');
-  textAnimation(text, 100, element);
-  removeText(100, element);
-}
-
-function writeText(text, duration, element) {
-  let i = 0;
-  let content = "";
-  let counter = 0;
-
-  let interval = setInterval(() => {
-    element.textContent = "";
-    content += text.charAt(i);
-    element.textContent = content + "|";
-    i++;
-    if (i >= text.length) {
-      clearInterval(interval);
-
-      setInterval(() => {
-        if (counter % 2 === 0) {
-          element.textContent = content + "\xA0";
-        } else {
-          element.textContent = content + "|";
-        }
-        counter++;
-      }, 200);
-    }
-  }, duration);
-}
-
-function removeText(duration, element) {
-  let i = 0;
-  let content = "";
-  let counter = 0;
-
-
+  let typed = new Typed("#headline", {
+    strings: ["virtual skiarea navigation^2000", "von Jonas Dorfinger^2000", "Medientechnick Projekt 2019^2000"],
+    typeSpeed: 50,
+    backSpeed: 40,
+    cursorChar: '|',
+    loop: true
+  });
 }
