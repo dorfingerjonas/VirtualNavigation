0910714243a5fe23869f845cf985cb6d6364db76
Jonas Dorfinger <36422131+dorfingerjonas@users.noreply.github.com>
2019-03-18 11:21:04 +0100

    fixed rs mistake

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 function init() {
   let typed = new Typed("#headline", {
-    strings: ["virtual skiarea navigation^2000", "von Jonas Dorfinger^2000", "Medientechnick Projekt 2019^2000", "virtual skiarea navigation^2000"],
+    strings: ["virtual skiarea navigation^2000", "von Jonas Dorfinger^2000", "Medientechnik Projekt 2019^2000", "virtual skiarea navigation^2000"],
     typeSpeed: 50,
     backSpeed: 40,
     cursorChar: '|',
