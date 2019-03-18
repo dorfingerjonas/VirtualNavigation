b3e156f72e28be9788dfc03b0f32cd91993cbde1
Jonas Dorfinger <36422131+dorfingerjonas@users.noreply.github.com>
2019-03-18 11:29:42 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function init() {
+  let typed = new Typed("#headline", {
+    strings: ["virtual skiarea navigation^2000", "von Jonas Dorfinger^2000", "Medientechnik Projekt 2019^2000", "virtual skiarea navigation^2000"],
+    typeSpeed: 50,
+    backSpeed: 40,
+    cursorChar: '|',
+  });
+}
