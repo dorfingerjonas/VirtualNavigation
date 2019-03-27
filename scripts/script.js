1a8c786d8fce8be5567e5ff7124b98a69245d595
Joonnnaas <jonas.dorfinger@gmx.at>
2019-03-27 12:46:09 +0100

    added interval for scroll Events

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,14 @@
-function init() {
 
-}
+window.addEventListener('load', () => {
+  let navTag = document.getElementById('nav');
+  
+  let interval =  setInterval(() => {
+    console.log(scrollY);
+
+    if (scrollY > 168.8) {
+      navTag.style.position = "fixed";
+    } else {
+      navTag.style.position = "relative"
+    }
+  }, 10);
+});
