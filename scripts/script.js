0dc26f4436b223c632afaab2040fdc0c666ef950
dorfingerjonas <jonas.dorfinger@gmx.at>
2019-03-27 13:35:30 +0100

    commented out

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,20 @@
 
 window.addEventListener('load', () => {
   let navTag = document.getElementById('nav');
-  
-  let interval =  setInterval(() => {
-    console.log(scrollY);
 
-    if (scrollY > 168.8) {
-      navTag.style.position = "fixed";
-    } else {
-      navTag.style.position = "relative"
-    }
-  }, 10);
+  // let interval =  setInterval(() => {
+  //   console.log(scrollY);
+  //
+  //   if (scrollY > 168.8) {
+  //     navTag.style.position = "sticky";
+  //   } else {
+  //     navTag.style.position = "relative"
+  //   }
+  // }, 10);
+
+  setInterval(function () {
+    scrollY += 20;
+    console.log(scrollY);
+  }, 250);
+
 });
