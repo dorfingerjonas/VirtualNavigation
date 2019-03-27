46d83cde17cd7a91af4751ac120297000f098b82
Joonnnaas <36422131+Joonnnaas@users.noreply.github.com>
2019-03-27 17:36:11 +0100

    removed console.log

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,6 @@ window.addEventListener('load', () => {
   let navTag = document.getElementById('nav');
 
   let interval =  setInterval(() => {
-    console.log(parseInt(scrollY));
 
     if (scrollY > 200) {
       navTag.style.boxShadow = '0px 7px 4px -2px rgba(184,184,184,1)';
