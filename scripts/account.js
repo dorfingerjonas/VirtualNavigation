35a82776cecd595db51da733fede304d1baf9b76
Joonnnaas <jonas.dorfinger@gmx.at>
2019-03-28 18:13:28 +0100

    added writeUserToDataBase() and getters to the values

--- scripts/account.js
+++ scripts/account.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', () => {
-  let config = {
+  const config = {
     apiKey: 'AIzaSyDHuGnlajKcr4s8_vn9XC5McqalXvWTBsg',
     authDomain: 'virtualskiareanavigation.firebaseapp.com',
     databaseURL: 'https://virtualskiareanavigation.firebaseio.com',
@@ -10,6 +10,8 @@ window.addEventListener('load', () => {
 
   firebase.initializeApp(config);
 
+  let database = firebase.database();
+
   const emailTxt = document.getElementById('email');
   const passwordTxt = document.getElementById('password');
   const loginBtn = document.getElementById('loginBtn');
@@ -19,8 +21,10 @@ window.addEventListener('load', () => {
   const usernamePopUp = document.getElementById('usernamePopUp');
   const accountArrow = document.getElementById('accountArrow');
   const accountPopUp = document.getElementById('accPopUp');
-  let vorname;
   let isAccountPopUpVisible = false;
+  let username;
+  let vorname;
+  let lastname;
 
   loginBtn.addEventListener('click', () => {
     console.log("login button pressed");
@@ -46,9 +50,11 @@ window.addEventListener('load', () => {
     const email = emailTxt.value;
     const password = passwordTxt.value;
     const auth = firebase.auth();
+    firstname = document.getElementById('firstname').value;
+    lastname = document.getElementById('lastname').value;
+    username = document.getElementById('username').value;
 
-    resetErrorField()
-
+    resetErrorField();
     const promise = auth.createUserWithEmailAndPassword(email, password);
     promise.catch((error) => {
 
@@ -56,13 +62,19 @@ window.addEventListener('load', () => {
 
       errField.style.display = "block";
       errField.textContent = error.message;
-
     });
+
+    let userId = firebase.auth().currentUser.uid;
+
+    console.log(userId);
+
+    writeUserToDatabase(firstname, lastname, username, email, userId);
   });
 
   logoutBtn.addEventListener('click', () => {
     firebase.auth().signOut().then(function() {
       // Sign-out successful.
+      accountPopUp.style.display = 'none';
       document.getElementById('usernameField').textContent = "";
     }).catch(function(error) {
       // An error happened.
@@ -74,7 +86,7 @@ window.addEventListener('load', () => {
     if (!isAccountPopUpVisible) {
       accountPopUp.style.display = "block";
       accountPopUp.style.top = "2vw";
-      accountPopUp.style.right = "10vw";
+      accountPopUp.style.right = "12vw";
       isAccountPopUpVisible = true;
     } else {
       accountPopUp.style.display = "none";
@@ -82,25 +94,73 @@ window.addEventListener('load', () => {
     }
   });
 
-// TODO:
-  // accountPopUp.style.display = "none";
-  // isAccountPopUpVisible = false;
-
-
-
-
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      vorname = document.getElementById('firstname').value;
       document.getElementById('user').textContent = user.email;
-      usernameField.textContent = vorname;
-      usernamePopUp.textContent = vorname;
+      getFirstname();
+      getUsername();
+
       accountArrow.style.display = "inline-block";
     } else {
       document.getElementById('user').textContent = "not logged in";
       accountArrow.style.display = "none";
     }
   });
+
+  const settings = document.getElementById('settings');
+  settings.addEventListener('click', () => {
+      window.location.href='./settings';
+      console.log('settings page opened');
+  });
+
+  function writeUserToDatabase(firstname, lastname, username, email, userId) {
+    firebase.database().ref('users/' + userId).set({
+      firstname: firstname,
+      lastname: lastname,
+      username: username,
+      email: email
+    }, (error) => {
+      if (error) {
+        // The write failed...
+        console.log("failed");
+      } else {
+        // Data saved successfully!
+        console.log('successful');
+      }
+    });
+  }
+
+  function getUsername() {
+    let userId = firebase.auth().currentUser.uid;
+
+    return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+      usernamePopUp.textContent = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+    });
+  }
+
+  function getFirstname() {
+    let userId = firebase.auth().currentUser.uid;
+
+    return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+      usernameField.textContent = (snapshot.val() && snapshot.val().firstname) || 'Anonymous';
+    });
+  }
+
+  function getLastname() {
+    let userId = firebase.auth().currentUser.uid;
+
+    return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+      let lastname = (snapshot.val() && snapshot.val().lastname) || 'Anonymous';
+    });
+  }
+
+  function getEmail() {
+    let userId = firebase.auth().currentUser.uid;
+
+    return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+      let email = (snapshot.val() && snapshot.val().email) || 'Anonymous';
+    });
+  }
 });
 
 function resetErrorField() {
