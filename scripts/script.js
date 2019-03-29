6131a64ad907605abfc7f755ce71b6f61b72d785
Joonnnaas <36422131+Joonnnaas@users.noreply.github.com>
2019-03-29 16:38:04 +0100

    changed if statement

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ window.addEventListener('load', () => {
 
   let interval =  setInterval(() => {
 
-    if (scrollY > 200) {
+    if (scrollY > 130) {
       navTag.style.boxShadow = '0px 7px 4px -2px rgba(184,184,184,1)';
     } else {
       navTag.style.boxShadow = '0px 0px 0px 0px rgba(184,184,184,1)';
