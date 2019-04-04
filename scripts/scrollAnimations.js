57898012d15d8e528d235b0ac8cf4e50ef2ea049
Joonnnaas <jonas.dorfinger@gmx.at>
2019-04-04 06:59:23 +0200

    added scroll transitions to nav and accountWindow

--- scripts/scrollAnimations.js
+++ scripts/scrollAnimations.js
@@ -1,12 +1,15 @@
 window.addEventListener('load', () => {
-  let navTag = document.getElementById('nav');
+  const navTag = document.getElementById('nav');
+  const accWndw = document.getElementById('accountWindow');
 
   let interval =  setInterval(() => {
 
     if (scrollY > 130) {
       navTag.style.boxShadow = '0px 7px 4px -2px rgba(184,184,184,1)';
+      accWndw.style.top = "0";
     } else {
       navTag.style.boxShadow = '0px 0px 0px 0px rgba(184,184,184,1)';
+      accWndw.style.top = '5vw';
     }
   }, 10);
 });
