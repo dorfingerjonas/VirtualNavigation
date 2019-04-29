bb6b73afe08e9d53bbd1afe42b67f4ce9505deb6
Dorfinger Jonas <it170133@WS8A01.htl-leonding.ac.at>
2019-04-29 11:46:52 +0200

    started Animation

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,3 +1,18 @@
 window.addEventListener('load', () => {
+  scrollTo(document.getElementById('nav').clientWidth / 2 - innerWidth / 2, 0);
+});
+window.addEventListener('keydown', (event) => {
+  if (event.key === 'ArrowRight') {
+    let originalPosition = scrollX;
 
+    event.preventDefault();
+
+    let interval = setInterval(() => {
+      scrollTo(scrollX + 1, 0);
+
+      if (scrollX === originalPosition + innerWidth) {
+        clearInterval(interval);
+      }
+    }, 10);
+  }
 });
