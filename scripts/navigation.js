3013c0ab675ee84e1a05d64dfa07f1d10f9bbf3a
Joonnnaas <jonas.dorfinger@gmx.at>
2019-04-30 21:38:45 +0200

    started navigationAnimation with keyboard

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,14 +1,32 @@
 window.addEventListener('load', () => {
   scrollTo(document.getElementById('nav').clientWidth / 2 - innerWidth / 2, 0);
 });
+
 window.addEventListener('keydown', (event) => {
+
+  const distanceToGo = document.getElementById('nav').childNodes[0].clientWidth;
+
+  console.log(parseInt(document.getElementById('nav').childNodes[0].clientWidth));
+
   if (event.key === 'ArrowRight') {
     let originalPosition = scrollX;
 
     event.preventDefault();
 
     let interval = setInterval(() => {
-      scrollTo(scrollX + 1, 0);
+      scrollTo(scrollX + distanceToGo, 0);
+
+      if (scrollX === originalPosition + innerWidth) {
+        clearInterval(interval);
+      }
+    }, 10);
+  } else if (event.key === 'ArrowLeft') {
+    let originalPosition = scrollX;
+
+    event.preventDefault();
+
+    let interval = setInterval(() => {
+      scrollTo(scrollX - distanceToGo, 0);
 
       if (scrollX === originalPosition + innerWidth) {
         clearInterval(interval);
