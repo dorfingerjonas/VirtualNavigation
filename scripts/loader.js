ed409f589b0069c4c10dfb19297c950d3531d842
Joonnnaas <36422131+Joonnnaas@users.noreply.github.com>
2019-05-26 16:20:00 +0200

    removed scroll mistake

--- scripts/loader.js
+++ scripts/loader.js
@@ -2,5 +2,6 @@ window.addEventListener('load', () => {
   setTimeout(() => {
     document.getElementById('loaderWrapper').style.display = 'none';
     document.getElementById('Home').style.opacity = 1;
+    document.getElementById('Home').style.display = 'block';
   }, Math.floor(Math.random() * 1500) + 1100);
 });
