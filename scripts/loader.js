a7862381e10ed48fe2e4d0c5b4d88b75e0d07930
Joonnnaas <36422131+Joonnnaas@users.noreply.github.com>
2019-05-26 16:11:19 +0200

    added Math.random() to loader

--- scripts/loader.js
+++ scripts/loader.js
@@ -2,5 +2,5 @@ window.addEventListener('load', () => {
   setTimeout(() => {
     document.getElementById('loaderWrapper').style.display = 'none';
     document.getElementById('Home').style.opacity = 1;
-  }, 2700);
+  }, Math.floor(Math.random() * 1500) + 1100);
 });
